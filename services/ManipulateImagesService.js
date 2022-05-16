fb21a77cc9e3897e6f6c1f2cd92541406e0ce796
Mateus Cardoso <serjumano17@gmail.com>
2022-05-16 20:22:40 -0300

    fix!: update of database after crop images

--- services/ManipulateImagesService.js
+++ services/ManipulateImagesService.js
@@ -13,6 +13,21 @@ dotenv.config();
 AWS.config.update({ region: process.env.REGION });
 const S3 = new AWS.S3({apiVersion: '2006-03-01'});
 
+function randomNumber(lowerLimit, higherLimit) {
+  let number = 1;
+  let random = Math.floor(Math.random() * higherLimit);
+  
+  while (random === 0 && random >= lowerLimit) {
+    random = Math.floor(Math.random() * higherLimit);
+  }
+
+  return random;
+}
+
+async function sleep(seconds) {
+  return new Promise(resolve => setTimeout(resolve, seconds * 1000));
+}
+
 async function cropImage(imageContent) {
   // initial pixel in x axis
   const left = 638;
@@ -20,7 +35,7 @@ async function cropImage(imageContent) {
   // 205
   const top = 220;
   const width = 645;
-  const height = 645;
+  const height = 635;
 
   return await sharp(imageContent)
     .extract({ left, top, width, height })
@@ -36,8 +51,11 @@ async function cropImage(imageContent) {
 async function cropImages(records) {
   console.log('chegou aqui');
   console.log('records', records);
+  let objectsToUpdate = [];
 
-  const promises = records.map(async (record, index) => {
+  for (let index = 0; index < records.length; index++) {
+    const record = records[index];
+    
     const { key } = record.s3.object;
     console.log('key', key);
     const getPayload = { Bucket: process.env.bucket, Key: key };
@@ -61,18 +79,73 @@ async function cropImages(records) {
     }).promise();
     const imageRef = String(result.Location);
 
+    const sleepTime = randomNumber(5, 25);
+    console.log(`esperando ${sleepTime} segundos`);
+    await sleep(sleepTime);
+
+    objectsToUpdate.push({
+      nameWithoutExtension,
+      imageRef,
+    });
+  }
+
+  for (let index = 0; index < objectsToUpdate.length; index++) {
+    const objectToUpdate = objectsToUpdate[index];
+
+    const sleepTime = randomNumber(5, 25);
+    console.log(`esperando ${sleepTime} segundos`);
+    await sleep(sleepTime);
+
+    console.log(`procurando o ${objectToUpdate.nameWithoutExtension}`);
+    console.log(`atualizando para ${objectToUpdate.imageRef}`);
+
     await Scraping.update({
-      source: imageRef
+      source: objectToUpdate.imageRef
     }, {
       where: {
         ref: {
-          [Op.like]: `%${nameWithoutExtension}%`
+          [Op.like]: `%${objectToUpdate.nameWithoutExtension}%`
         }
       }
     })
-  })
+  }
 
-  await Promise.all(promises);
+  // const promises = records.map(async (record, index) => {
+  //   const { key } = record.s3.object;
+  //   console.log('key', key);
+  //   const getPayload = { Bucket: process.env.bucket, Key: key };
+  //   console.log('get payload', getPayload);
+  //   const image = await S3.getObject(getPayload).promise();
+  //   const imageContent = image.Body;
+
+  //   console.log('Cropping image:', key);
+
+  //   const croppedImageBuffer = await cropImage(imageContent);
+  //   const nameWithoutExtension = path.basename(key, path.extname(key));
+  //   const newKey = `${nameWithoutExtension}.jpg`;
+    
+  //   console.log('image cropped:', newKey);
+
+  //   const result = await S3.upload({
+  //     Bucket: process.env.bucket,
+  //     Key: newKey,
+  //     Body: croppedImageBuffer,
+  //     ContentType: 'image/jpeg',
+  //   }).promise();
+  //   const imageRef = String(result.Location);
+
+  //   await Scraping.update({
+  //     source: imageRef
+  //   }, {
+  //     where: {
+  //       ref: {
+  //         [Op.like]: `%${nameWithoutExtension}%`
+  //       }
+  //     }
+  //   })
+  // })
+
+  // await Promise.all(promises);
 }
 
 module.exports = {
